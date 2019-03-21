1652797b808a23c072d86902f647a9b1bd1e1280
Vladislav Marchyk <37183107+MaskaSaw@users.noreply.github.com>
2019-03-21 19:59:38 +0300

    MergeSort implemented

--- NET.W.Marchyk.2019.01/NET.W.Marchyk.2019.01/NET.W.Marchyk.2019.01/SortingMethods.cs
+++ NET.W.Marchyk.2019.01/NET.W.Marchyk.2019.01/NET.W.Marchyk.2019.01/SortingMethods.cs
@@ -9,6 +9,80 @@ namespace Day01
     public static class SortingMethods
     {
         /// <summary>
+        /// This method represents a solution for MergeSort algorithm.
+        /// </summary>
+        /// <param name="inputArray">This is a sortable array.</param>
+        /// <returns>Returns a sorted array.</returns>
+        public static int[] MergeSort(int[] inputArray)
+        {
+            if (inputArray == null)
+                throw new ArgumentNullException();
+            if (inputArray.Length == 0)
+                throw new ArgumentException();
+
+            if (inputArray.Length == 1)
+                return inputArray;
+            int center = inputArray.Length / 2;
+            int[] arr1 = new int[center];
+            int i;
+            for (i = 0; i < arr1.Length; i++)
+                arr1[i] = inputArray[i];
+            int[] arr2 = new int[inputArray.Length - center];
+            for (int j = 0; j < arr2.Length; j++)
+            {
+                arr2[j] = inputArray[i];
+                i++;
+            }
+            return Merge(MergeSort(arr1), MergeSort(arr2));
+
+
+        }
+        /// <summary>
+        /// This is a side method for the MergeSort.
+        /// </summary>
+        /// <param name="arr1">The left sorted array part.</param>
+        /// <param name="arr2">The sorted array part.</param>
+        /// <returns>Returns the sorted array part.</returns>
+        private static int[] Merge(int[] arr1, int[] arr2)
+        {
+            int id1 = 0;
+            int id2 = 0;
+            int[] merged = new int[arr1.Length + arr2.Length];
+            for (int i = 0; i < merged.Length; i++)
+            {
+                if (id1 < arr1.Length && id2 < arr2.Length)
+                {
+                    if (arr1[id1] > arr2[id2] && id2 < arr2.Length)
+                    {
+                        merged[i] = arr2[id2];
+                        id2++;
+                    }
+                    else
+                    {
+                        merged[i] = arr1[id1];
+                        id1++;
+                    }
+                }
+                else
+                {
+                    if (id2 < arr2.Length)
+                    {
+                        merged[i] = arr2[id2];
+                        id2++;
+
+                    }
+                    else
+                    {
+                        merged[i] = arr1[id1];
+                        id1++;
+                    }
+                }
+            }
+
+            return merged;
+        }
+
+        /// <summary>
         /// This method represents a solutiion for QuickSort algorythm.
         /// </summary>
         /// <param name="inputArray">Array to sort.</param>
@@ -51,11 +125,20 @@ namespace Day01
             return inputArray;
         }
 
+
+        /// <summary>
+        /// This is a side method for the MergeSort.
+        /// </summary>
+        /// <param name="inputArray">The array to change.</param>
+        /// <param name="arr1">First element index.</param>
+        /// <param name="arr2">Second element index.</param>
         public static void Swap(ref int[] inputArray, int i, int j)
         {
             int buf = inputArray[i];
             inputArray[i] = inputArray[j];
             inputArray[j] = buf;
         }
+
+        
     }
 }
