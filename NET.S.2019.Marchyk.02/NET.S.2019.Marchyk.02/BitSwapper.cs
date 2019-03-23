f7e8c78a8519af5d2b93a58fdd638da716d781ae
Vladislav Marchyk <37183107+MaskaSaw@users.noreply.github.com>
2019-03-23 13:11:47 +0300

    First try

---
+++ NET.S.2019.Marchyk.02/NET.S.2019.Marchyk.02/BitSwapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Marchyk._02
+{
+    public class BitSwapper
+    {
+        public static int InsertNumber(int inputNumber, int insertNumber, int i, int j)
+        {
+            BitArray inputNumberBits = new BitArray(new int[] { inputNumber });
+            bool[] bits = new bool[inputNumberBits.Count];
+            inputNumberBits.CopyTo(bits, 0);
+            byte[] bitValues = bits.Select(bit => (byte)(bit ? 1 : 0)).ToArray();
+
+            return 0;
+        }
+    }
+}
---
+++ NET.S.2019.Marchyk.02/NET.S.2019.Marchyk.02/Tests/NUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET.S._2019.Marchyk._02.Tests
+{
+    [TestFixture]
+    class NUnitTests
+    {
+        [Test]
+        public void DebugTest()
+        {
+            Assert.AreEqual(1, BitSwapper.InsertNumber(10, 1, 1, 1));
+        }
+    }
+}
