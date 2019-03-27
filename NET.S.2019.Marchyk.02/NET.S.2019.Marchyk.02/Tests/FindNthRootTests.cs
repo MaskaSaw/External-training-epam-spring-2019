66f14523a3ac9fc53a3ea6e9104c8842850f50fe
Vladislav Marchyk <37183107+MaskaSaw@users.noreply.github.com>
2019-03-27 10:06:13 +0300

    Test fixes

--- NET.S.2019.Marchyk.02/NET.S.2019.Marchyk.02/Tests/FindNthRootTests.cs
+++ NET.S.2019.Marchyk.02/NET.S.2019.Marchyk.02/Tests/FindNthRootTests.cs
@@ -4,17 +4,23 @@ using NUnit.Framework;
 namespace NET.S._2019.Marchyk._02.Tests
 {
     [TestFixture]
-    class FindNthRootTests
+    public class FindNthRootTests
     {
-        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
-        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
-        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
-        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
-        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
-        public double FindNthRoot(double number, int power, double accuracy) => BiggerNumberFInder.FindNthRoot(number, power, accuracy);
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1,  0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+
+        public void FindNthRootTest(double number, int power, double accuracy, double expected)
+        {
+            double actual = BiggerNumberFInder.FindNthRoot(number, power, accuracy);
+            Assert.AreEqual(expected, actual, accuracy);
+        }
+      ///  public double FindNthRoot(double number, int power, double accuracy) => BiggerNumberFInder.FindNthRoot(number, power, accuracy);
     }
 }
