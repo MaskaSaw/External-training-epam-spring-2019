083204901f3bc3b5b4fba4152dd90de088beb823
Vladislav Marchyk <37183107+MaskaSaw@users.noreply.github.com>
2019-04-18 19:39:32 +0300

    Added logging

--- NET.S.2019.Marchyk.08/NET.S.2019.Marchyk.08/Console/Program.cs
+++ NET.S.2019.Marchyk.08/NET.S.2019.Marchyk.08/Console/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Books;
 using BookService;
 using BookStorage;
+using Logs;
 
 namespace ConsoleProgram
 
@@ -15,24 +16,32 @@ namespace ConsoleProgram
     {
         static void Main(string[] args)
         {
-            BookListStorage bookStorage = new BookListStorage("NewStorage.bin");
-            BookListService bookService = new BookListService(bookStorage);
+            try
+            {
+                BookListStorage bookStorage = new BookListStorage("NewStorage.bin");
+                BookListService bookService = new BookListService(bookStorage);
 
-            bookService.AddBookToShop(new Book("978-3-16-123451-0", "Ivanov", "one", "Minsk", 2000, 1000, 100));
-            bookService.AddBookToShop(new Book("978-3-16-123452-1", "Petrov", "two", "Gomel", 2001, 2000, 200));
-            bookService.AddBookToShop(new Book("978-3-16-123453-2", "Glebov", "three", "Brest", 2002, 3000, 300));
-            bookService.AddBookToShop(new Book("978-3-16-123454-3", "Arkhipov", "four", "Vitebsk", 2003, 4000, 400));
+                bookService.AddBookToShop(new Book("978-3-16-123451-0", "Ivanov", "one", "Minsk", 2000, 1000, 100));
+                bookService.AddBookToShop(new Book("978-3-16-123452-1", "Petrov", "two", "Gomel", 2001, 2000, 200));
+                bookService.AddBookToShop(new Book("978-3-16-123453-2", "Glebov", "three", "Brest", 2002, 3000, 300));
+                bookService.AddBookToShop(new Book("978-3-16-123454-3", "Arkhipov", "four", "Vitebsk", 2003, 4000, 400));
 
-            var books = new List<Book>();
-            books.Add(bookService.FindBook(new FindBookByName("one", bookService.GetAllBooks())));
-            PrintBook(books);
+                var books = new List<Book>();
+                books.Add(bookService.FindBook(new FindBookByName("one", bookService.GetAllBooks())));
+                PrintBook(books);
 
-            bookService.Sort(null);
-            PrintBook(bookService.GetAllBooks());
+                bookService.Sort(null);
+                PrintBook(bookService.GetAllBooks());
 
-            Console.WriteLine(books[0].ToString("2", CultureInfo.CurrentCulture));
-            bookService.Save();
-            Console.ReadKey();
+                Console.WriteLine(books[0].ToString("2", CultureInfo.CurrentCulture));
+                bookService.Save();
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Log.Write(ex);
+            }
+            
         }
 
         public static void PrintBook(List<Book> books)
