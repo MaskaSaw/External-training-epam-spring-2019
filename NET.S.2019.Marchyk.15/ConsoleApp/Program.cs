0faf249484b3233c4ee5cecf4ab5a2959dc53f2f
Vladislav Marchyk <37183107+MaskaSaw@users.noreply.github.com>
2019-05-08 20:16:46 +0300

    Bank account implemented + console app tested

---
+++ NET.S.2019.Marchyk.15/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Operations;
+using BankAccount;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>();
+            AccountOperations accountService = new AccountOperations();
+
+            accountService.CreateAccount(new Account(111, "Ivan", "Ivanov", 100m, 5, AccountType.Base));
+            accountService.CreateAccount(new Account(112, "Petr", "Petrov", 1250m, 40, AccountType.Gold));
+            accountService.CreateAccount(new Account(113, "Sidor", "Sidorov", 10245m, 120, AccountType.Premium));
+
+            PrintAll(accountService.GetAllAccounts());
+
+            accountService.AddAmount(111, 500m);
+            PrintAll(accountService.GetAllAccounts());
+
+            Console.ReadKey();
+        }
+
+        private static void PrintAll(List<Account> accounts)
+        {
+            foreach (var acc in accounts)
+            {
+                Console.WriteLine(acc.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ NET.S.2019.Marchyk.15/Storage/AccountStorage.cs
@@ -0,0 +1,80 @@
+﻿using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Storage
+{
+    public class AccountStorage
+    {
+      
+        private const string Path = "D:\\Folder";
+       
+        public List<Account> ReadAccountFromFile()
+        {
+            var accounts = new List<Account>();
+            using (var br = new BinaryReader(File.Open(Path, FileMode.OpenOrCreate,
+                FileAccess.Read, FileShare.Read)))
+            {
+                while (br.BaseStream.Position != br.BaseStream.Length)
+                {
+                    var account = Reader(br);
+                    accounts.Add(account);
+                }
+            }
+
+            return accounts;
+        }
+
+       
+        public void AppendAccountToFile(Account account)
+        {
+            using (var bw = new BinaryWriter(File.Open(Path, FileMode.Append,
+                FileAccess.Write, FileShare.None)))
+            {
+                Writer(bw, account);
+            }
+        }
+
+       
+        public void OverWriteFile(IEnumerable<Account> accounts)
+        {
+            using (var bw = new BinaryWriter(File.Open(Path, FileMode.Create,
+                FileAccess.Write, FileShare.None)))
+            {
+                foreach (var account in accounts)
+                    Writer(bw, account);
+            }
+        }
+
+        private static void Writer(BinaryWriter binary, Account account)
+        {
+            binary.Write(account.Id.ToString());
+            binary.Write(account.FirstName);
+            binary.Write(account.LastName);
+            binary.Write(account.Amount);
+            binary.Write(account.Points);
+            binary.Write(account.Status.ToString());
+            binary.Write(account.Type.ToString());
+
+        }
+
+        private static Account Reader(BinaryReader binary)
+        {
+            var id = binary.ReadString();
+            var firstName = binary.ReadString();
+            var lastName = binary.ReadString();
+            var amount = binary.ReadDecimal();
+            var points = binary.ReadInt32();
+            var status = binary.ReadString();
+            var type = binary.ReadString();
+
+            return new Account(Convert.ToInt32(id), firstName, lastName, amount, points, (AccountType)Enum.Parse(typeof(AccountType), type))
+            {
+                Status = (AccountStatus)Enum.Parse(typeof(AccountStatus), status)
+            };
+
+        }
+
+    }
+}
